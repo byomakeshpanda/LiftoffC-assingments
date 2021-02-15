206caf5d39386404a6db09efa34841a1532188a6
Byomakesh Panda <78654237+byomakeshpanda@users.noreply.github.com>
2021-02-15 13:54:15 +0530

    Week 2 solutions

--- Q1.c
+++ Q1.c
@@ -1,15 +1,13 @@
 #include <stdio.h>
-void main ()
+void main()
 {
-	int a,b,sum,diff,mult;
-	float divi;
-	printf("Enter 2 numbers\n");
-	scanf("%d %d",&a,&b);
-	sum=a+b;
-	diff=a-b;
-	mult=a*b;
-	divi=a*1.0/b;
-	printf("\nSum = %d\nDiffernce = %d\nProduct = %d\nDivision = %f",sum,diff,mult,divi);
-	
+	int inp,rem,s=0;
+	printf("Enter a number\n");
+	scanf("%d",&inp);
+	for ( ;inp>0;inp=inp/10)
+	{
+		rem = inp%10;
+		s=s+rem;
+	}
+	printf("Sum of the digits is %d",s);
 }
-
